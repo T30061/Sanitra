702b478341b5c6ddcbe1cca0bd329cfbacbdbb9f
Teddy <30061@htltraun.at>
2025-01-29 14:14:10 +0100

    Init x1

---
+++ src/main/java/htl/teddy_com/Main.java
@@ -0,0 +1,9 @@
+package htl.teddy_com;
+
+public class Main{
+    //EinE app wie Web untis und die auch ziemlich ähnlich arbeiten soll und auch mir Schüler app usw....
+    //eigenes INterface für  lehrer und joa
+    public static void main(String[] args) {
+
+    }
+}
